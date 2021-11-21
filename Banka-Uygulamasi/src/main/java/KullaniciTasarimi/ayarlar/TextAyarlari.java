19105674c42d099a3e861fba788828f12743b58d
Murat Acun <83479146+Acun9@users.noreply.github.com>
2021-11-21 15:19:19 +0300

    Giriş Ekranı Kodlanması
    Arkaplan ve yazı rengi ayarları, text odaklanma ayarları, Key Ayarları, Buton actions, frame açıldığında yeniden düzenlemeler yapabilmek için işlemler yapıldı.

---
+++ Banka-Uygulamasi/src/main/java/KullaniciTasarimi/ayarlar/TextAyarlari.java
@@ -0,0 +1,59 @@
+
+package KullaniciTasarimi.ayarlar;
+
+import java.awt.Color;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JTextField;
+
+public class TextAyarlari {
+    
+    /*
+    text odaklanma ayarları
+    */
+    
+    private static String OrijinalText;
+    private static Color OrijinalFgColor;
+    
+    
+    public static void chechTheTextFocusGained(JTextField textField,String org){
+        OrijinalText = org;
+        if(textField.getText().trim().equals(org)){
+            OrijinalFgColor = textField.getForeground();
+            textField.setText("");
+        }       
+        textField.setForeground(OrijinalFgColor);
+    }
+    
+    
+    public static void chechTheTextFocusLost(JTextField textField){
+        if(textField.getText().trim().equals("")){
+            textField.setText(OrijinalText);
+            textField.setForeground(OrijinalFgColor);
+        }
+        else {
+            textField.setForeground(Color.BLACK);
+        }
+        textField.setForeground(OrijinalFgColor);
+    }
+    
+    /*
+    Key Ayarları
+    */
+    
+    public static void setOnlyNumber(JTextField textField){
+        textField.addKeyListener(new KeyAdapter(){
+            
+            @Override
+            public void keyTyped(KeyEvent e) {
+                char c = e.getKeyChar();
+                if(!Character.isDigit(c)){
+                    e.consume();
+                }
+            }
+            
+        });
+        
+    }
+}
+
